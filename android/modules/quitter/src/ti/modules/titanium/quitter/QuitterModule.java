450621522ed8949f7908ef6f0d0f1a33cf32c41f
Joel Meador <joel@expectedbehavior.com>
2010-07-01 16:54:40 -0400

    quitter module start

---
+++ android/modules/quitter/src/ti/modules/titanium/quitter/QuitterModule.java
@@ -0,0 +1,19 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2009-2010 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+package ti.modules.titanium.quitter;
+
+import org.appcelerator.titanium.TiContext;
+import org.appcelerator.titanium.TiDict;
+import org.appcelerator.titanium.TiModule;
+import org.appcelerator.titanium.util.TiConvert;
+
+public class QuitterModule extends TiModule {
+
+	public QuitterModule(TiContext context) {
+		super(context);
+	}
+}
